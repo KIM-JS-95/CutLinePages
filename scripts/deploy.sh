9b258f61c76032f68c1fd69087835fce4a27e4da
KIM-JS-95 <baugh248730@gmail.com>
2021-10-11 12:10:23 +0900

    test'

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec2-user/CutLinePages
+REPOSITORY=/home/ec2-user
+PROJECTNAME=CutLinePages
 
-cd $REPOSITORY
+cd $REPOSITORY/$PROJECTNAME
 
 echo "> Git Pull"
 
@@ -18,22 +19,22 @@ cp ./build/libs/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -f CutLinePages)
+CURRENT_PID=$(pgrep -f ${PROJECTNAME}*.jar)
 
 echo "$CURRENT_PID"
 
 if [ -z $CURRENT_PID ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-    echo "> kill -9 $CURRENT_PID"
-    kill -9 $CURRENT_PID
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
     sleep 5
 fi
 
 echo "> 새 어플리케이션 배포"
 
-JAR_NAME=$(ls $REPOSITORY/ |grep 'Gallary' | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep *.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
-nohup java -jar $REPOSITORY/$JAR_NAME &
+nohup java -jar $REPOSITORY/$JAR_NAME 2>&1 &
