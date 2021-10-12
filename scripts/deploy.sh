01a1d3428d3076adadf41153116a314712eedefe
KIM-JS-95 <baugh248730@gmail.com>
2021-10-13 01:13:23 +0900

    git add .

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,8 @@
 REPOSITORY=/home/ec2-user
 PROJECTNAME=CutLinePages
 
+PROJECT_TITLE=Gallary
+
 cd $REPOSITORY/$PROJECTNAME
 
 echo "> Git Pull"
@@ -19,9 +21,11 @@ cp ./build/libs/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -f ${PROJECTNAME}*.jar)
+CURRENT_PID=$(pgrep -f Gallary)
+
 
 echo "$CURRENT_PID"
+
 
 if [ -z $CURRENT_PID ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
@@ -29,11 +33,12 @@ else
     echo "> kill -15 $CURRENT_PID"
     kill -15 $CURRENT_PID
     sleep 5
+
 fi
 
 echo "> 새 어플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/ | grep *.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep 'Gallary' | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
