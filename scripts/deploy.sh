328160535194f311365ec153d13eb44cb69dccc3
KIM-JS-95 <baugh248730@gmail.com>
2021-10-14 16:31:09 +0900

    git add .

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,45 +1,28 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec2-user
-PROJECTNAME=CutLinePages
+BUILD_JAR=$(ls /home/ec2-user/jenkins/build/libs/*.jar)     # jar가 위치하는 곳
+JAR_NAME=$(basename $BUILD_JAR)
 
-PROJECT_TITLE=Gallary
+echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/deploy.log
 
-cd $REPOSITORY/$PROJECTNAME
+echo "> build 파일 복사" >> /home/ec2-user/deploy.log
 
-#echo "> Git Pull"
-#
-#git pull
-#
-#echo "> 프로젝트 Build 시작"
-#
-#./gradlew build
-#
-#echo "> Build 파일 복사"
+DEPLOY_PATH=/home/ec2-user/
 
-cp ./build/libs/*.jar $REPOSITORY/jenkins
+cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 구동중인 애플리케이션 pid 확인"
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/deploy.log
+CURRENT_PID=$(pgrep -f $JAR_NAME)
 
-CURRENT_PID=$(pgrep -f Gallary)
-
-
-echo "$CURRENT_PID"
-
-
-if [ -z $CURRENT_PID ]; then
-    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+if [ -z $CURRENT_PID ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/deploy.log
 else
-    echo "> kill -15 $CURRENT_PID"
-    kill -15 $CURRENT_PID
-    sleep 5
-
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
 fi
 
-echo "> 새 어플리케이션 배포"
-
-JAR_NAME=$(ls -tr $REPOSITORY/ | grep 'Gallary' | tail -n 1)
-
-echo "> JAR Name: $JAR_NAME"
-
-nohup java -jar $REPOSITORY/$JAR_NAME 2>&1 &
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/deploy.log
+nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/deploy_err.log &
