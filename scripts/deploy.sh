35a5ca5d3e85a0ba44aae3fe20a572d686cfcb5e
KIM-JS-95 <baugh248730@gmail.com>
2021-11-18 16:55:55 +0900

    123

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -34,3 +34,11 @@ else
     sleep 5
 
 fi
+
+echo "> 새 어플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/jenkins/ | grep 'Gallary' | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+nohup java -jar $REPOSITORY/jenkins/$JAR_NAME 2>&1 &
