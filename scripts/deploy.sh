dbe3b834fb774f0ae11cfc5bcbde8241acf31102
KIM-JS-95 <baugh248730@gmail.com>
2021-11-21 20:29:48 +0900

    123

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,16 +28,13 @@ CURRENT_PID=$(pgrep -f Gallary)
 
 echo "$CURRENT_PID"
 
-
 if [ -z $CURRENT_PID ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
     echo "> kill -15 $CURRENT_PID"
     kill -15 $CURRENT_PID
     sleep 5
-
 fi
-
 
 echo "> 새 어플리케이션 배포"
 
@@ -45,4 +42,4 @@ JAR_NAME=$(ls -tr $REPOSITORY/jenkins/ | grep 'Gallary' | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
-nohup java -jar $REPOSITORY/jenkins/$JAR_NAME 2>&1 &
+nohup java -jar $JAR_NAME > $REPOSITORY/jenkins/nohup.out 2>&1 &
