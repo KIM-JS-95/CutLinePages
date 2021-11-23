577ff14e9746f16d8ed607c2977ac3e6bdfc1bc6
KIM-JS-95 <baugh248730@gmail.com>
2021-11-23 23:15:35 +0900

    123

--- src/main/java/com/myGallary/mycontroller/MemberController.java
+++ src/main/java/com/myGallary/mycontroller/MemberController.java
@@ -16,13 +16,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "www.cutlinepage.ml:8081")
 @Controller
 public class MemberController {
 
